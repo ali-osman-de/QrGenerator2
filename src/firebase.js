a0294fad256bd3c364c7644a4c8cb44e750dceb8
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2024-11-05 12:00:47 +0300

    .

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAESbSNXNwrwZMdaE4wCUEywCkf7Jkpigc",
-  authDomain: "qrcreatorbyunique.firebaseapp.com",
-  projectId: "qrcreatorbyunique",
-  storageBucket: "qrcreatorbyunique.appspot.com",
-  messagingSenderId: "531810809757",
-  appId: "1:531810809757:web:5f0b4a236a80ed2a3ce174",
-  measurementId: "G-TRGSXFXH3J",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
